abcf507b5722b8fbd10ba91e361fddfc526ced78
palrang22 <kseunghui@gmail.com>
2024-03-23 22:40:32 +0900

    Shazam_second_page

--- shazam_clone/lib/main.dart
+++ shazam_clone/lib/main.dart
@@ -34,49 +34,13 @@ class _HomepageState extends State<Homepage> {
             TabBarView(
               children: [
                 FirstTab(),
-                FirstTab(),
+                SecondTab(),
                 FirstTab(),
               ],
-            ),
-            SafeArea(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.topCenter,
-                      child: TabPage(),
-                    ),
-                  ],
-                ),
-              ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class TabPage extends StatefulWidget {
-  const TabPage({super.key});
-
-  @override
-  State<TabPage> createState() => _TabPageState();
-}
-
-class _TabPageState extends State<TabPage> {
-  @override
-  Widget build(BuildContext context) {
-    return TabPageSelector(
-      color: DefaultTabController.of(context).index == 1
-          ? Colors.blue[300]
-          : Colors.grey[400],
-      selectedColor: DefaultTabController.of(context).index == 1
-          ? Colors.white
-          : Colors.blue,
-      indicatorSize: 8,
     );
   }
 }
@@ -134,12 +98,20 @@ class FirstTab extends StatelessWidget {
             Text("라이브러리"),
           ],
         ),
-        actions: [],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            TabPageSelector(
+              color: DefaultTabController.of(context).index == 1
+                  ? Colors.blue[300]
+                  : Colors.grey[400],
+              selectedColor: DefaultTabController.of(context).index == 1
+                  ? Colors.white
+                  : Colors.blue,
+              indicatorSize: 8,
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
@@ -210,6 +182,7 @@ class FirstTab extends StatelessWidget {
               child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
+                  childAspectRatio: 4 / 5,
                 ),
                 itemCount: songs.length,
                 itemBuilder: (BuildContext context, int index) {
@@ -219,29 +192,187 @@ class FirstTab extends StatelessWidget {
                   String artist = song['artist']!;
 
                   index % 2 == 0;
+
                   return Container(
+                    margin: EdgeInsets.only(
+                      left: 4,
+                      right: 4,
+                      bottom: 4,
+                      top: 4,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            blurRadius: 1,
+                            spreadRadius: 1,
+                          )
+                        ]),
                     child: Column(
                       children: [
-                        Image.network(
-                          imageUrl,
-                        ),
-                        Text(
-                          title,
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w300,
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            topRight: Radius.circular(8),
+                          ),
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        Text(artist,
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ))
+                        SizedBox(height: 15),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                title,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(artist,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  )),
+                              Spacer(),
+                              Image.network(
+                                "https://i.ibb.co/KG9m5QS/applemusic.png",
+                                width: 60,
+                              ),
+                              Spacer(),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   );
                 },
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondTab extends StatelessWidget {
+  const SecondTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Icon(
+                Icons.person_rounded,
+                color: Colors.white,
+              ),
+              Text("라이브러리",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 8,
+                  )),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Icon(
+                  Icons.stacked_bar_chart,
+                  color: Colors.white,
+                ),
+                Text("차트",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 98, 200, 248),
+              Color.fromARGB(255, 39, 102, 239)
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            TabPageSelector(
+              color: DefaultTabController.of(context).index == 1
+                  ? Colors.blue[300]
+                  : Colors.grey[400],
+              selectedColor: DefaultTabController.of(context).index == 1
+                  ? Colors.white
+                  : Colors.blue,
+              indicatorSize: 8,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "Shazam하려면 탭하세요",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+                alignment: Alignment.center,
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 130, 189, 238),
+                  shape: BoxShape.circle,
+                ),
+                child: Image.network(
+                  "https://i.ibb.co/hxNbZ8p/shazam.png",
+                  color: Colors.white,
+                  width: 100,
+                  height: 100,
+                )),
+            SizedBox(height: 70),
+            Container(
+              alignment: Alignment.center,
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 130, 189, 238),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            )
           ],
         ),
       ),
